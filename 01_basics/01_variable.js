0a3ee97e2e19929e6e5b3b18a0a47b5991197d74
Dipak choudhary <95021330+dipakk07@users.noreply.github.com>
2023-10-10 11:39:38 +0000

    dataTypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,9 @@
+const accName="Dipak"
+let accId = "dipak@12.com"
+accPassword="1235"
+let accCity;
+console.log(accPassword)
+console.table([accName, accId, accPassword, accCity])
+accId="12234"
+console.log(accId)
+// don't use "var" due to block scope , go for the using of "let" keyword
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // Treat all js code as newer version
+
+// alert("hello") can't use , we have different syntax to use alert in node 
+
+// alert(3+4) // can't use becoz we are using node not browser 
+
+console.log(3+3)
+console.log("hello")
+
+let Name="Dipak"
+let age=18
+let logIn = true
+let State= null //(empty)
+
+// number => range 2 to the power 53
+// bigint
+// string = "" or ''
+// boolean = true/false
+// null=> standAlone value
+// undefined => declare but not define
+// symbol // to find unique  
+
+
+console.log(typeof "Dipak")
+console.log(typeof age)
+console.log(typeof undefined)
+console.log(typeof null)  // object
+console.log(typeof logIn)
