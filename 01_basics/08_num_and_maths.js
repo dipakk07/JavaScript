cc23ad65f10ae773e485d0aeb8488d19ff2af0d7
Dipak choudhary <95021330+dipakk07@users.noreply.github.com>
2023-10-11 11:03:46 +0000

    NUMBER AND MATH FUNCTION

---
+++ 01_basics/08_num_and_maths.js
@@ -0,0 +1,14 @@
+
+let Num1= new Number(100)
+console.log(Num1)
+
+let num2=2000000
+console.log(num2)
+console.log(num2.toLocaleString('en-IN'))
+console.log("_____________________")
+
+// ++++++++++++++ Math +++++++++++++
+
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1))+min) // random number generator from 10 to 20(n)
