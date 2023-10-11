7ccc0c5b62ba5c7d77d0c43c692843e85b15204f
Dipak choudhary <95021330+dipakk07@users.noreply.github.com>
2023-10-11 06:53:22 +0000

    modified datatype conversion

--- 01_basics/03_ConversionOperator.js
+++ 01_basics/03_ConversionOperator.js
@@ -35,3 +35,31 @@ console.log("8\t"+NumberLoggedIn)
 let StringLoggedIn = String(LoggedIn)  // Boolean to String Conversion
 console.log(typeof(StringLoggedIn))
 console.log("9\t"+ StringLoggedIn)
+
+console.log(2**3) //it will print 2 to the power 3 => 8
+
+console.log("1" + 2 + 2)    // => 122
+console.log(1 + 2 +"2")     // => 32
+console.log("1"+ 2+ "3")    // => 123
+console.log(1+ "2"+ 3 )    //  => 123
+
+let num1 = 23
+let num2 = 34
+let num3 = num1 + num2 
+console.log(num3)
+
+let num4 =num5=num6= 2+2 // => value of num4,num5, and num6 will be 4 (but it is bad habbit (masssy code))
+console.log(num4)
+console.log(num5)
+console.log(num6)
+
+let GameCounter= 100
+console.log(GameCounter)  // =>100
+GameCounter++
+console.log(GameCounter)  // => 101
+++GameCounter
+console.log(GameCounter)  // => 102
+GameCounter--
+console.log(GameCounter) // => 101
+--GameCounter
+console.log(GameCounter)  // => 100
