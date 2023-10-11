769527f58e00076a6a6c17156d738c1eb7b32c19
Dipak choudhary <95021330+dipakk07@users.noreply.github.com>
2023-10-11 06:21:13 +0000

    Data types Conversion complete

---
+++ 01_basics/03_ConversionOperator.js
@@ -0,0 +1,37 @@
+let MyName= "Dipak123"
+console.log(typeof(MyName))
+console.log("1 \t"+ MyName)
+
+let booleanDipak = Boolean(MyName)  // String to boolean conversion
+console.log(typeof(booleanDipak))
+console.log("2 \t"+ booleanDipak)
+
+let NumberDipak= Number(MyName)    // String To Number Conversion
+console.log(typeof(NumberDipak))
+console.log("3 \t"+ NumberDipak )
+
+
+let AnyNumber= 99
+console.log("4 \t"+ AnyNumber)
+console.log(typeof(99))
+
+let StringAnyNumebr = String(AnyNumber) // Number to String Conversion
+console.log(typeof(StringAnyNumebr))
+console.log("5\t" + StringAnyNumebr)
+
+let BooleanAnyNumber= Boolean(AnyNumber) // Number to Boolean Conversion
+console.log(typeof(BooleanAnyNumber))
+console.log("6\t"+ BooleanAnyNumber)
+
+let LoggedIn = true
+console.log(typeof(LoggedIn))
+console.log("7\t"+LoggedIn)
+
+
+let NumberLoggedIn = Number(LoggedIn) // Boolean to Number Conversion
+console.log(typeof(NumberLoggedIn))
+console.log("8\t"+NumberLoggedIn)
+
+let StringLoggedIn = String(LoggedIn)  // Boolean to String Conversion
+console.log(typeof(StringLoggedIn))
+console.log("9\t"+ StringLoggedIn)
