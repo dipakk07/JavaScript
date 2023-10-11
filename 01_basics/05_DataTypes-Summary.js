5c823dabeed7768798b0b38e5a75012ece73ea46
Dipak choudhary <95021330+dipakk07@users.noreply.github.com>
2023-10-11 08:48:47 +0000

    Data type summary

---
+++ 01_basics/05_DataTypes-Summary.js
@@ -0,0 +1,13 @@
+// Primitive DAta Type 
+   // 7 Types : Number , String , Boolean , undefined, null, Symbol, BigInt
+
+// Non primitive data type  : Array, Object, Function (object function)
+
+const id= Symbol('123')
+const AnotherId=Symbol('123')
+console.log(AnotherId==id)
+console.log(AnotherId)
+console.log(id)
+
+const heros =["dipak","yuvan"]
+console.log(typeof(heros))
